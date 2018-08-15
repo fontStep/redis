42515e2787722bb32bc20b4c9fa917f9924417a5
xiaowjw <271466204@qq.com>
2018-08-15 08:51:09 +0800

    提交

---
+++ src/main/java/wjw/redis/pojo/UserPO.java
@@ -0,0 +1,53 @@
+package wjw.redis.pojo;
+
+import javax.print.DocFlavor;
+import java.io.Serializable;
+
+/**
+ * @ClassName: UserPO
+ * @Description: TOOD
+ * @version: v1.0.0
+ * @author: wjw
+ * @date: 2018/8/14 16:04
+ */
+public class UserPO {
+
+    private Integer id;
+
+    private String name;
+
+    private Integer sex;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "UserPO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", sex=" + sex +
+                '}';
+    }
+}
--- src/test/java/wjw/redis/RedisApplicationTests.java
+++ src/test/java/wjw/redis/RedisApplicationTests.java
@@ -7,6 +7,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
+import wjw.redis.pojo.UserPO;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,10 +25,74 @@ public class RedisApplicationTests {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    /**
+    * @Description: 操作String类型 set
+    * @param:
+    * @return 
+    * @version: v1.0.0
+    * @exception
+    * @author: wjw
+    * @date: 2018/8/14 15:44 
+    */
     @Test
     public void set(){
         redisTemplate.opsForValue().set("test:set","testValue1");
     }
 
+    /**
+    * @Description: 获取所有key
+    * @param:
+    * @return 
+    * @version: v1.0.0
+    * @exception
+    * @author: wjw
+    * @date: 2018/8/14 15:44 
+    */
+    @Test
+    public void getKeys(){
+        Set<String> set = redisTemplate.keys("*");
+        for (String str : set) {
+            System.out.println(str);
+        }
+    }
 
+    /**
+    * @Description: 操作列表
+    * @param:
+    * @return 
+    * @version: v1.0.0
+    * @exception
+    * @author: wjw
+    * @date: 2018/8/14 15:45 
+    */
+    @Test
+    public void list(){
+        List<String> list1 = new ArrayList<>(10);
+        list1.add("a");
+        list1.add("b");
+        list1.add("c");
+        list1.add("d");
+        Long  result=  redisTemplate.opsForList().rightPush("list1",list1);
+        System.out.println(result);
+    }
+
+    @Test
+    public void getList(){
+       List<String> result = (List<String>) redisTemplate.opsForList().rightPop("list1");
+       if(null!=result && result.size()>0){
+           for (int i =0 ;i<result.size();i++){
+               System.out.println(result.get(i));
+           }
+
+       }
+    }
+
+    @Test
+    public void setHash(){
+        UserPO userPO = new UserPO();
+        userPO.setId(2);
+        userPO.setName("b");
+        userPO.setSex(0);
+        redisTemplate.opsForHash().put("student",userPO.getId()+"",userPO);
+    }
 }
